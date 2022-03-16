e374172ce491cad7d11e0966c3ff8ec720f1bf54
Văn Tú Nguyễn <vantu@Vans-MBP.fritz.box>
2022-03-16 19:16:11 +0100

    starter spring boot

---
+++ src/main/java/de/tumteam/cloudphotosstorage/CloudPhotosStorageApplication.java
@@ -0,0 +1,13 @@
+package de.tumteam.cloudphotosstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudPhotosStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudPhotosStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/tumteam/cloudphotosstorage/CloudPhotosStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tumteam.cloudphotosstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudPhotosStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
